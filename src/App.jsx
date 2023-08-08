39446fddde514640f0f66822db705a4db550a8d1
kmaharajan30 <kmaharajan30@gmail.com>
2023-08-08 18:49:42 +0530

    pagination

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,34 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchTodos } from "./todoSlice";
+import { clickCurrentPage, clickNext, clickPrev, fetchTodos } from "./todoSlice";
 import Card from 'react-bootstrap/Card';
 
 function App() {
 
   const dispatch = useDispatch();
-  const { todos } = useSelector((state) => state.todo);
-  console.log(todos);
+  const { todos, todosPerPage, currentPage } = useSelector((state) => state.todo);
+  const totalPages = todos.length / todosPerPage;
+  const pages = [...Array(totalPages+1).keys()].slice(1);
+  console.log(pages);
+  const indexLast = currentPage * todosPerPage;
+  const indexFirst = indexLast - todosPerPage; 
+
+  const finalTodos = todos.slice(indexFirst, indexLast);
+
+  const navigatePrev =()=>{
+    if(currentPage !==1){
+      dispatch(clickPrev());
+    }
+  }
+  const navigateNext = () =>{
+    if(currentPage !== totalPages){
+      dispatch(clickNext());
+    }
+  }
+  const currentPages = (page)=>{
+    dispatch(clickCurrentPage(page));
+  }
 
   return (
     <div className="App">
@@ -24,10 +44,15 @@ function App() {
         >
           Click here!
         </Button>
+        
       </div>
+      <hr />
+        <div className="text-center">
+          Page {currentPage} of {totalPages}
+        </div>
       <div className="row mb-3 my-3 mx-3">
         {todos &&
-          todos.map((todo, index) => (
+          finalTodos.map((todo, index) => (
             <div key={index} className="col-md-3">
               <Card style={{ width: "14rem" }}>
                 <Card.Body>
@@ -36,6 +61,16 @@ function App() {
               </Card>
             </div>
           ))}
+      </div>
+
+      <div className="text-center mt-5">
+        <p>
+          <span className="button" onClick={navigatePrev}> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l160 160c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L109.2 288 416 288c17.7 0 32-14.3 32-32s-14.3-32-32-32l-306.7 0L214.6 118.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-160 160z"/></svg> </span>
+          {pages.map((page,index)=>(
+            <span className="button" onClick={()=>currentPages(page)} key={index}>{page}</span>
+          ))}
+          <span className="button" onClick={navigateNext}> <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg></span>
+        </p>
       </div>
     </div>
   );
--- src/todoSlice.js
+++ src/todoSlice.js
@@ -10,12 +10,25 @@ export const allTodos = createSlice({
     name:"todos",
     initialState:{
         todos:[],
+        todosPerPage:20,
+        currentPage:1,
     },
     extraReducers:{
         [fetchTodos.fulfilled]:(state,action)=>{
             state.todos = action.payload;
         }
+    },
+    reducers:{
+        clickNext : (state, action) => {
+            state.currentPage++;
+        },
+        clickPrev : (state) => {
+            state.currentPage--;
+        },
+        clickCurrentPage : (state, action) => {
+            state.currentPage= action.payload;
+        },
     }
 });
-
+export const {clickNext, clickPrev, clickCurrentPage} = allTodos.actions;
 export default allTodos.reducer;
